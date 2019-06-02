1b6bc8621af74920ccd7878375635227a109d4ea
YeJingjing <yjjjng0901@163.com>
2019-06-02 13:32:36 +0800

    add resources

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,11 +5,25 @@
 </template>
 
 <style lang="less">
+.size{
+  width: 100%;
+  height: 100%;
+}
+html,body{
+  .size;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-size: 16px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  .size;
 }
 </style>
